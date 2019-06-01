bc6a05e099a8d7b366708639a695f47a0911b42c
Frederik Reiter <kontakt@frederikreiter.de>
2019-06-02 00:15:54 +0200

    Add basic portfolio

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+import json
+
+from flask import Flask, render_template
+from datetime import date, datetime
 
 app = Flask(__name__)
 
+all_entries = []
+
+
+@app.before_first_request
+def load_portfolio_entries():
+    global all_entries
+    with open("portfolio.json") as f:
+        all_entries = json.loads(f.read())
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def portfolio():
+    days = (datetime.now().date()-date(2002, 11, 21)).days
+    age = {"days": days, "years": round(days/365.2425, 1)}
+    return render_template("portfolio.html", entries=all_entries, age=age)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
